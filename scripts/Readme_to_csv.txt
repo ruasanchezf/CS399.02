e8211a1d277eb641503466ba08945901beb9ddd9
Rudra Subramanian <subramanianr@carleton.edu>
2021-10-17 18:18:56 -0400

    script 1

---
+++ scripts/Readme_to_csv.txt
@@ -0,0 +1,4 @@
+puts dest ip, source ip, size, and time of pcap into csv
+csv is the same name as the pcap
+the pcap is an argument taken in when running the program
+
---
+++ scripts/to_csv.py
@@ -0,0 +1,31 @@
+import pyshark
+import os
+import sys
+import csv
+
+#takes input for file name
+pcap = pyshark.FileCapture(sys.argv[1])
+filename = sys.argv[1][:-4] + "csv"
+#removes csv file if already exists
+os.system("rm -r "+filename)
+#opening csv file
+csvF = open(filename, 'w')
+writer = csv.writer(csvF)
+writer.writerow(["source IP", "Destination IP", "File size", "Time"])
+#iterates through packets
+for packet in pcap:
+    try: #some packets dont have destination or source ip not sure why. 
+        #All packets that dont have source also dont have destination
+        dest = packet.ip.dst
+        src = packet.ip.src
+    except AttributeError:
+        dest = "-"
+        src = "-"
+    size = len(packet)
+    time = str(packet.sniff_time)
+    #making row and writing to csv
+    row = [src, dest, size, time]
+    writer.writerow(row)
+csvF.close()
+
+
